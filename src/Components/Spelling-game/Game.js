0ba4a3e704e5e86fc791f1bc72840b6f6cf9e09e
singhashwani003 <123533223+singhashwani003@users.noreply.github.com>
2023-10-27 09:45:01 +0530

    changes on spelling game

--- src/Components/Spelling-game/Game.js
+++ src/Components/Spelling-game/Game.js
@@ -60,7 +60,10 @@ const Game = () => {
     
     return null;
   };
-
+  const handleRefreshClick = () => {
+    // Reload the page
+    window.location.reload();
+  };
   const handleButtonClick = (event) => {
     const buttonValue = event.target.value;
     console.log(typeof remainingchar);
@@ -126,8 +129,8 @@ const Game = () => {
                 <button type="button" class="key" value="Y" onClick={handleButtonClick}>Y</button>
                 <button type="button" class="key" value="Z" onClick={handleButtonClick}>Z</button>  
               </div>
-              <button id="sucess" className="btn btn-success text-light py-3 w-50 fw-bold mt-5" style={{display:"none",cursor:"auto"}}>Congrats! You guessed ALGEBRA in {attempt} attempts</button>
-              <button className='btn btn-primary w-25 btn-md mt-4 fw-bold rounded-pill'>Refresh</button>
+              <button id="sucess" className="btn btn-success text-light py-3 fw-bold mt-5" style={{display:"none",cursor:"auto"}}>Congrats! You guessed ALGEBRA in {attempt} attempts</button>
+              <button className='btn btn-primary w-25 btn-md mt-4 fw-bold rounded-pill' onClick={handleRefreshClick}>Refresh</button>
           </div>
           </div>
           <div className='col-lg-3 col-sm-12 mt-4 ps-2 score'>
